fe0eabfb4860ef0390150ccca28f609fd756cd87
zohaib <zohaib.buyer@gmail.com>
2013-07-09 06:38:34 -0400

    added new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funstar::Application.config.secret_token = '5c8fff76dbc3295e71f2b447f05d2cd0b083a938beae2b0988d7e72886a725e4958c70b5078506eb3b6be603ba8f8a3219b0f2b46889aeebf08d26ed5c9fdf7c'
